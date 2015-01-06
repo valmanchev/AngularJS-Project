ba1e0fc1d0d32b26fef9196f189241bb91b2ffe8
valmanchev <vmanchev@yahoo.com>
2015-01-06 18:49:05 +0200

    added paging

--- app/js/controllers/HomeController.js
+++ app/js/controllers/HomeController.js
@@ -1,15 +1,24 @@
 'use strict';
 
 app.controller('HomeController',
-    function ($scope, adsService, notifyService) {
-        adsService.getAds(
-            null,
-            function success(data) {
+   function ($scope, adsService, notifyService, pageSize) {
+      $scope.adsParams = {
+          'startPage' : 1,
+          'pageSize' : pageSize
+      };
+
+      $scope.reloadAds = function() {
+          adsService.getAds(
+              $scope.adsParams,
+              function success(data) {
                 $scope.ads = data;
-            },
-            function error(err) {
+              },
+              function error(err) {
                 notifyService.showError("Cannot load ads", err);
-            }
-        );
-    }
+              }
+          );
+      };
+
+      $scope.reloadAds();
+   }
 );
