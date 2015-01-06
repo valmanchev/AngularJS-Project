2390f4bf4538fa290ee95a965561307c3a8f1a15
valmanchev <vmanchev@yahoo.com>
2015-01-06 17:19:43 +0200

    Defined a RightSidebarController.js

---
+++ app/js/controllers/RightSidebarController.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// The RightSidebarController controls the content displayed in the right sidebar
+app.controller('RightSidebarController',
+    function ($scope, categoriesService, townsService) {
+      // TODO
+      $scope.categoriesService = categoriesService;
+      $scope.townsService = townsService;
+    }
+);
