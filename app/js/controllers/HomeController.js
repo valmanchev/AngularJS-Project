94cc1ff55986f8057854a99e82916d35df7341e7
valmanchev <vmanchev@yahoo.com>
2015-01-06 17:56:09 +0200

    Added code to your HomeController.js

--- app/js/controllers/HomeController.js
+++ app/js/controllers/HomeController.js
@@ -3,9 +3,23 @@
 // The HomeController holds the presentation logic for the home screen
 app.controller('HomeController',
     function ($scope, $rootScope, adsService, notifyService, pageSize) {
+	    function ($scope, adsService, notifyService) {
+	        adsService.getAds(
+	            null,
+	            function success(data) {
+	                // TODO: put the ads in the scope
+	            },
+	            function error(err) {
+	                notifyService.showError("Cannot load ads", err);
+	            }
+	        );
+	    }
+
+
       // TODO
-      $scope.adsService = adsService;
-      $scope.notifyService = notifyService;
-      $scope.pageSize = pageSize;
+      //$scope.adsService = adsService;
+      //$scope.notifyService = notifyService;
+      //$scope.pageSize = pageSize;
    }
+
 );
