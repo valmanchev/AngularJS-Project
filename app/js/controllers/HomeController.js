59acca0ee1ec3244b57288961f94478797944a95
valmanchev <vmanchev@yahoo.com>
2015-01-06 18:19:08 +0200

    fixed bugs in HomeController.js

--- app/js/controllers/HomeController.js
+++ app/js/controllers/HomeController.js
@@ -1,25 +1,15 @@
 'use strict';
 
-// The HomeController holds the presentation logic for the home screen
 app.controller('HomeController',
-    function ($scope, $rootScope, adsService, notifyService, pageSize) {
-	    function ($scope, adsService, notifyService) {
-	        adsService.getAds(
-	            null,
-	            function success(data) {
-	                // TODO: put the ads in the scope
-	            },
-	            function error(err) {
-	                notifyService.showError("Cannot load ads", err);
-	            }
-	        );
-	    }
-
-
-      // TODO
-      //$scope.adsService = adsService;
-      //$scope.notifyService = notifyService;
-      //$scope.pageSize = pageSize;
-   }
-
+    function ($scope, adsService, notifyService) {
+        adsService.getAds(
+            null,
+            function success(data) {
+                $scope.ads = data;
+            },
+            function error(err) {
+                notifyService.showError("Cannot load ads", err);
+            }
+        );
+    }
 );
