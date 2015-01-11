f93a971a73772392938877fef3497f944904932e
valmanchev <vmanchev@yahoo.com>
2015-01-11 07:46:36 +0200

    Implemented "Image Upload with Live Preview

--- app/js/controllers/UserPublishNewAdController.js
+++ app/js/controllers/UserPublishNewAdController.js
@@ -22,5 +22,20 @@ app.controller('UserPublishNewAdController',
               }
           );
       };
+      $scope.fileSelected = function(fileInputField) {
+          delete $scope.adData.imageDataUrl;
+          var file = fileInputField.files[0];
+          if (file.type.match(/image\/.*/)) {
+              var reader = new FileReader();
+              reader.onload = function() {
+                  $scope.adData.imageDataUrl = reader.result;
+                  $(".image-box").html("<img src='" + reader.result + "'>");
+              };
+              reader.readAsDataURL(file);
+          } else {
+              $(".image-box").html("<p>File type not supported!</p>");
+          }
+      };
+
   }
 );
